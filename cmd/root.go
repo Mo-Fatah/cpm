84e1a69e8efa367a5fb511128f2934f21f9c96c6
mohamed <mohamedabdelfatah2027@gmail.com>
2023-03-21 15:06:42 +0200

    initial commit
    Signed-off-by: mohamed <mohamedabdelfatah2027@gmail.com>

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cpm",
+	Short: "Career Page Monitor",
+	Long: `Career Page Monitor is a CLI tool to monitor new job postings.
+cpm helps you keep track of the latest job postings of companies you are interested in`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) { cmd.Help() },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cpm.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Mohamed Abdelfatah <mohamedabdelfatah2027@gmail.com>
+*/
+package main
+
+import "github.com/Mo-Fatah/cpm/cmd"
+
+func main() {
+	cmd.Execute()
+}
